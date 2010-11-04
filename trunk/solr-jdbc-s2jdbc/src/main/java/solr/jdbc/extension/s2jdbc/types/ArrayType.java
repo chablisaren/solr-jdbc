5ced92387e2caaf4296dac0a6ce9a42dbb8b3de7
kawasima1016 <kawasima1016@7dff0303-d3db-17cf-4a76-943c28e989de>
2010-11-04 05:36:33 +0000

    add feature : S2JDBC Array type

---
+++ trunk/solr-jdbc-s2jdbc/src/main/java/solr/jdbc/extension/s2jdbc/types/ArrayType.java
@@ -0,0 +1,71 @@
+package solr.jdbc.extension.s2jdbc.types;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.apache.commons.lang.StringUtils;
+import org.seasar.extension.jdbc.types.AbstractValueType;
+import org.seasar.extension.jdbc.types.ValueTypes;
+import org.seasar.extension.jdbc.util.BindVariableUtil;
+
+public class ArrayType extends AbstractValueType {
+	public ArrayType() {
+		super(Types.ARRAY);
+	}
+	
+	@Override
+	public Object getValue(ResultSet resultSet, int index) throws SQLException{
+		return (Object[])resultSet.getArray(index).getArray();
+	}
+	
+	@Override
+	public Object getValue(ResultSet resultSet, String columnName)
+		throws SQLException {
+		return (Object[])resultSet.getArray(columnName).getArray();
+	}
+	
+	@Override
+	public void bindValue(PreparedStatement ps, int index, Object value) throws SQLException {
+		if (value == null) {
+			setNull(ps, index);
+		} else {
+			ps.setObject(index, value);
+		}
+	}
+	
+	@Override
+	public void bindValue(CallableStatement cs, String parameterName,
+			Object value) throws SQLException {
+		// TODO
+	}
+
+	@Override
+	public Object getValue(CallableStatement cs, int index) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getValue(CallableStatement cs, String parameterName)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toText(Object value) {
+		if(value == null) {
+			return BindVariableUtil.nullText();
+		}
+		if(!value.getClass().isArray()) {
+			return "<not array>";
+		}
+		Object[] array = (Object[]) value;
+		return "[" + StringUtils.join(array, ',');
+	}
+
+
+}
--- trunk/solr-jdbc-s2jdbc/src/test/java/solr/jdbc/extension/s2jdbc/S2JDBCTest.java
+++ trunk/solr-jdbc-s2jdbc/src/test/java/solr/jdbc/extension/s2jdbc/S2JDBCTest.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import javax.transaction.UserTransaction;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.seasar.extension.jdbc.EntityMetaFactory;
 import org.seasar.extension.jdbc.JdbcManager;
 import org.seasar.extension.unit.S2TestCase;
 import org.seasar.framework.beans.util.BeanMap;
+import org.seasar.framework.util.tiger.CollectionsUtil;
 
 import solr.jdbc.extension.s2jdbc.entity.Player;
+import solr.jdbc.extension.s2jdbc.meta.SolrPropertyMetaFactoryImpl;
 import solr.jdbc.extension.s2jdbc.service.PlayerService;
 
 public class S2JDBCTest extends S2TestCase {
@@ -23,12 +27,17 @@ public class S2JDBCTest extends S2TestCase {
 
 	
 	public void testSearchSimple() throws Exception {
+		EntityMetaFactory entityMetaFactory = (EntityMetaFactory)getComponent("entityMetaFactory");
+		entityMetaFactory.getEntityMeta(Player.class);
 		jdbcManager.updateBySql("DROP TABLE PLAYER").execute();
-		jdbcManager.updateBySql("CREATE TABLE PLAYER(PLAYER_ID number, TEAM varchar(10), PLAYER_NAME varchar(50), REGISTERED_AT DATE)").execute();
+		jdbcManager.updateBySql("CREATE TABLE PLAYER(PLAYER_ID number, TEAM varchar(10), PLAYER_NAME varchar(50), POSITION varchar(10) ARRAY, REGISTERED_AT DATE)").execute();
 		Player player1 = new Player();
 		player1.playerId = 1;
 		player1.playerName = "高橋慶彦";
 		player1.team = "カープ";
+		player1.position = CollectionsUtil.newArrayList();
+		player1.position.add("遊撃手");
+		player1.position.add("二塁手");
 		userTransaction.begin();
 		playerService.insert(player1);
 		userTransaction.commit();
@@ -38,6 +47,7 @@ public class S2JDBCTest extends S2TestCase {
 		Player player = playerList.get(0);
 		assertEquals("Entityに値が入っている", "高橋慶彦", player.playerName);
 		assertNull("設定しなかったところはnullが入る", player.registeredAt);
+		assertEquals("マルチバリューの項目にListが入る", 2, player.position.size());
 		
 		player.playerName = "野村謙二郎";
 		int res = playerService.update(player);
