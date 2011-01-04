f787252eb535132e23d2d0b0c89d5b135644e0da
kawasima1016@gmail.com <kawasima1016@gmail.com@7dff0303-d3db-17cf-4a76-943c28e989de>
2011-01-04 02:10:56 +0000

    [maven-release-plugin]  copy for tag solr-jdbc-0.1.0

---
+++ tags/solr-jdbc-0.1.0/src/main/java/com/google/code/solr_jdbc/impl/CollectionResultSet.java
@@ -0,0 +1,47 @@
+package com.google.code.solr_jdbc.impl;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+
+import com.google.code.solr_jdbc.message.DbException;
+import com.google.code.solr_jdbc.message.ErrorCode;
+
+public class CollectionResultSet extends AbstractResultSet {
+	private List<String> columns;
+
+	public CollectionResultSet() {
+		this.docList = new SolrDocumentList();
+		this.columns = new ArrayList<String>();
+	}
+
+	protected void add(List<Object> record) {
+		SolrDocument doc = new SolrDocument();
+		for (int i=0; i<columns.size(); i++) {
+			doc.setField(columns.get(i), record.get(i));
+		}
+		docList.add(doc);
+	}
+
+	protected void setColumns(List<String> columnNames) {
+		this.columns = columnNames;
+	}
+	@Override
+	public int findColumn(String columnLabel) throws SQLException {
+		int i=0;
+		for(; i<columns.size(); i++) {
+			if (StringUtils.equals(columnLabel, columns.get(i))) {
+				break;
+			}
+		}
+		if(i==columns.size()) {
+			throw DbException.get(ErrorCode.COLUMN_NOT_FOUND, columnLabel);
+		}
+		return i;
+	}
+
+}
---
+++ tags/solr-jdbc-0.1.0/src/main/java/com/google/code/solr_jdbc/parser/ItemListParser.java
@@ -0,0 +1,66 @@
+package com.google.code.solr_jdbc.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
+import net.sf.jsqlparser.expression.operators.relational.ItemsListVisitor;
+import net.sf.jsqlparser.statement.select.SubSelect;
+
+import com.google.code.solr_jdbc.expression.Item;
+import com.google.code.solr_jdbc.expression.Literal;
+import com.google.code.solr_jdbc.expression.Parameter;
+import com.google.code.solr_jdbc.expression.ValueExpression;
+import com.google.code.solr_jdbc.message.DbException;
+import com.google.code.solr_jdbc.message.ErrorCode;
+
+public class ItemListParser implements ItemsListVisitor{
+	private List<Parameter> parameters;
+	private List<Item> itemList;
+
+	public ItemListParser() {
+		this.itemList = new ArrayList<Item>();
+		this.parameters = new ArrayList<Parameter>();
+	}
+	
+	public ItemListParser(List<Parameter> parameters) {
+		this();
+		this.parameters.addAll(parameters);
+	}
+	
+	@Override
+	public void visit(SubSelect subselect) {
+		throw DbException.get(ErrorCode.FEATURE_NOT_SUPPORTED, "SubQuery");
+		
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void visit(ExpressionList expressionList) {
+		ExpressionParser expressionParser = new ExpressionParser();
+		for(Expression expression : (List<Expression>)expressionList.getExpressions()) {
+			expression.accept(expressionParser);
+			if (expressionParser.isParameter()) {
+				Parameter p = new Parameter(parameters.size());
+				parameters.add(p);
+				itemList.add(p);
+			} else {
+				if (expressionParser.getExpression() instanceof ValueExpression) {
+					itemList.add(new Literal(((ValueExpression)expressionParser.getExpression()).getValue()));
+				} else {
+					DbException.get(ErrorCode.FEATURE_NOT_SUPPORTED, "itemList must be literal.");
+				}
+			}
+		}
+		
+	}
+
+	public List<Parameter> getParameters() {
+		return parameters;
+	}
+
+	public List<Item> getItemList() {
+		return itemList;
+	}
+}
