9aed02622ce6f86ed1ce0fa08f47d0dddf5913b0
kawasima1016 <kawasima1016@7dff0303-d3db-17cf-4a76-943c28e989de>
2011-04-08 09:29:51 +0000

    検索機能を作る

--- solr-jdbc/src/main/java/com/google/code/solr_jdbc/impl/EmbeddedConnectionImpl.java
+++ solr-jdbc/src/main/java/com/google/code/solr_jdbc/impl/EmbeddedConnectionImpl.java
@@ -2,15 +2,18 @@ package com.google.code.solr_jdbc.impl;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.core.CoreContainer;
 import org.apache.solr.core.CoreDescriptor;
 import org.apache.solr.core.SolrConfig;
 import org.apache.solr.core.SolrCore;
 import org.apache.solr.schema.IndexSchema;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 
@@ -18,21 +21,40 @@ import org.xml.sax.SAXException;
 public class EmbeddedConnectionImpl extends SolrConnection {
 	private CoreContainer coreContainer;
 	private int timeout = 0;
-	
+
+	private static Properties defaults;
+	static {
+		defaults = new Properties();
+		defaults.put("DATA_DIR", "target/solrdata");
+		defaults.put("SOLR_HOME", "target/test-classes");
+	}
+
+	private Properties options = new Properties(defaults);
+
 	public EmbeddedConnectionImpl(String serverUrl)
 			throws ParserConfigurationException, IOException, SAXException {
 		super(serverUrl);
-		coreContainer = new CoreContainer("target/solrdata");
-		SolrConfig solrConfig = new SolrConfig("target/solrdata", new FileInputStream("target/test-classes/solrconfig.xml"));
-		IndexSchema indexSchema = new IndexSchema(solrConfig, serverUrl, new FileInputStream("target/test-classes/schema.xml"));
-		CoreDescriptor coreDescriptor = new CoreDescriptor(coreContainer, "", 
+		String[] tokens = serverUrl.split(";");
+		if(tokens != null && tokens.length > 1) {
+			tokens = (String[])ArrayUtils.remove(tokens, 0);
+			for (String token : tokens) {
+				String[] params = token.split("=", 2);
+				options.put(params[0], params[1]);
+			}
+		}
+		coreContainer = new CoreContainer(options.getProperty("DATA_DIR"));
+		SolrConfig solrConfig = new SolrConfig(options.getProperty("DATA_DIR"),
+				new InputSource(new FileInputStream(options.getProperty("SOLR_HOME")+"/solrconfig.xml")));
+		IndexSchema indexSchema = new IndexSchema(solrConfig, serverUrl,
+				new InputSource(new FileInputStream(options.getProperty("SOLR_HOME")+"/schema.xml")));
+		CoreDescriptor coreDescriptor = new CoreDescriptor(coreContainer, "",
 				solrConfig.getResourceLoader().getInstanceDir());
-		SolrCore core = new SolrCore(null, "target/solrData", solrConfig, indexSchema, coreDescriptor);
+		SolrCore core = new SolrCore(null, options.getProperty("DATA_DIR"), solrConfig, indexSchema, coreDescriptor);
 		coreContainer.register("", core, false);
 		EmbeddedSolrServer solrServer = new EmbeddedSolrServer(coreContainer, "");
 		setSolrServer(solrServer);
 	}
-	
+
 	@Override
 	public void close() {
 		coreContainer.shutdown();
@@ -42,7 +64,7 @@ public class EmbeddedConnectionImpl extends SolrConnection {
 	public int getQueryTimeout() {
 		return timeout;
 	}
-	
+
 	@Override
 	public void setQueryTimeout(int timeout) {
 		this.timeout = timeout;
