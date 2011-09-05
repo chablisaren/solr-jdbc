deb077ec8e8cd3ab6c39670afba63cea9fd6a57f
kawasima1016@gmail.com <kawasima1016@gmail.com@7dff0303-d3db-17cf-4a76-943c28e989de>
2011-09-05 02:02:49 +0000

    [maven-release-plugin]  copy for tag solr-jdbc-0.1.3

---
+++ tags/solr-jdbc-0.1.3/solr-jdbc/src/main/java/com/google/code/solr_jdbc/value/SolrType.java
@@ -0,0 +1,18 @@
+package com.google.code.solr_jdbc.value;
+
+public enum SolrType {
+	NULL,
+	STRING,
+	DECIMAL,
+	TEXT,
+	DATE,
+	BOOLEAN,
+	INT,
+	SHORT,
+	BYTE,
+	LONG,
+	FLOAT,
+	DOUBLE,
+	ARRAY,
+	UNKNOWN,
+}
